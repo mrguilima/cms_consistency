d2f9c503ac7ce1338dc259d608a8d187e9e14893
Guilherme Lima <lima@fnal.gov>
2025-12-10 11:31:30 -0600

    Update procedure to build monitor website image

--- monitor/server/build.sh
+++ monitor/server/build.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-docker build -t rucio-con-mon .
+set -e
+
+export MONITOR_VERSION=rucio-con-mon:1.0.2
+export HARBOR=registry.cern.ch/cmsrucio
+
+podman build -t $HARBOR/$MONITOR_VERSION .
+podman push $HARBOR/$MONITOR_VERSION
+
 

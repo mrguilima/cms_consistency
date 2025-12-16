76b657057f351e8394e4dca0b8e47289ca152405
Guilherme Lima <lima@fnal.gov>
2025-12-16 13:25:54 -0600

    con-mon webpage image testing

--- monitor/server/start.sh
+++ monitor/server/start.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-echo start.sh: version 2
+echo start.sh: version 1.0.2
 
 CC_DATA=/reports
 WM_DATA=/reports/unmerged
 
-cd ~
 echo "--- starting ---"
-ls -l
-ls -l app
-
-echo "--- $CC_DATA: ---"
-ls -ld $CC_DATA
-#ls -l $CC_DATA
-
-echo "--- $WM_DATA: ---"
-ls -ld $WM_DATA
-#ls -l $WM_DATA
-
+cd /app
 python -V
 
-echo "--- starting server with: " python app/server.py "$@" 8400 $CC_DATA $WM_DATA
+echo "--- starting server with: " python app/server.py --um-ignore /store/unmerged/logs/ "$@" 8400 $CC_DATA $WM_DATA
 python app/server.py --um-ignore /store/unmerged/logs/ "$@" 8400 $CC_DATA $WM_DATA
